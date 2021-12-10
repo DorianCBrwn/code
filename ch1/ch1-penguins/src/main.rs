001fe8d330087307194c41f9c132d441088ac518
Dorian Brown <brown.3794@gmail.com>
2021-12-10 11:14:16 -0500

    add comments to code

--- ch1/ch1-penguins/src/main.rs
+++ ch1/ch1-penguins/src/main.rs
@@ -1,4 +1,5 @@
-fn main() {                 // <1> <2>
+fn main() {
+  // <1> <2>
   let penguin_data = "\
   common name,length (cm)
   Little penguin,33
@@ -9,24 +10,37 @@ fn main() {                 // <1> <2>
 
   let records = penguin_data.lines();
 
+  // Common Control Flow Mechanisms
   for (i, record) in records.enumerate() {
-    if i == 0 || record.trim().len() == 0 {  // <3>
+    //Skips header row and lines with only whitespace
+    if i == 0 || record.trim().len() == 0 {
+      // <3>
       continue;
     }
 
-    let fields: Vec<_> = record     // <4>
-      .split(',')                   // <5>
-      .map(|field| field.trim())    // <6>
-      .collect();                   // <7>
+    //Method syntax
+    //Starts with a line of text
+    let fields: Vec<_> = record // <4>
+      // Splits records in fields
+      .split(',') // <5>
+      //Trims whitespace of each field
+      .map(|field| field.trim()) // <6>
+      //Builds a collection of fields
+      .collect(); // <7>
 
-    if cfg!(debug_assertions) {              // <8>
-      eprintln!("debug: {:?} -> {:?}",
-	             record, fields);            // <9>
+    //checks configuration at compile time
+    if cfg!(debug_assertions) {
+      // <8>
+      //prints to standard error
+      eprintln!("debug: {:?} -> {:?}", record, fields); // <9>
     }
 
     let name = fields[0];
-    if let Ok(length) = fields[1].parse::<f32>() { // <10>
-        println!("{}, {}cm", name, length);        // <11>
+    //Attempts to parse fields as a floating point number
+    if let Ok(length) = fields[1].parse::<f32>() {
+      // <10>
+      //Prints to standard out
+      println!("{}, {}cm", name, length); // <11>
     }
   }
 }
